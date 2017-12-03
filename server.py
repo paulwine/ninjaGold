d6bff11cef1af15ce7276a082718c9044222646b
Paul <paulwinegard@gmail.com>
2017-12-02 19:21:02 -0800

    Finalized

--- server.py
+++ server.py
@@ -42,31 +42,34 @@ def addCoins():
         return redirect("/")
     elif session["name"] == "casino":
         session["num"] = random.randint(-50,50)
-        if session["num"] < 0:
-            flash("Stop gambling, you have a Problem.  You lose " + str(session["num"]) + " coins.")
-            print("Stop gambling, you have a Problem.  You lose " + str(session["num"]) + " coins.")
-            session["wallet"] += session["num"]
-            flash("You have " + str(session["wallet"]) +" coins in your wallet")
-            print("You have " + str(session["wallet"]) +" coins in your wallet")
-            
-        elif session["num"] > 0:
-            flash("You lucky Dog.  You win " + str(session["num"]) + " coins.")
-            print("You lucky Dog.  You win " + str(session["num"]) + " coins.")
-            session["wallet"] += session["num"]
-            flash("You have " + str(session["wallet"]) +" coins in your wallet")
-            print("You have " + str(session["wallet"]) +" coins in your wallet")
-        elif session["num"] == 0:
-            flash("You broke even.  No coins for you")
-            print("You broke even.  No coins for you")
-            session["wallet"] += session["num"]
-            flash("You have " + str(session["wallet"]) +" coins in your wallet")
-            print("You have " + str(session["wallet"]) +" coins in your wallet")
+        if session["wallet"] <= 0:
+            flash("You have no more money.  The casino people and loan sharks are going to track you down and break your knees and your family will live in poverty.  Were all those drunken nights at the casino really worth it?  You were such an honorable ninja, and now you have nothing.")
+            print("You have no more money.  The casino people and loan sharks are going to track you down and break your knees and your family will live in poverty.  Were all those drunken nights at the casino really worth it?  You were such an honorable ninja, and now you have nothing.")
             return redirect("/")
+        elif session["wallet"]>0:
+            if session["num"] < 0:
+                flash("Stop gambling, you have a Problem.  You lose " + str(session["num"]) + " coins.")
+                print("Stop gambling, you have a Problem.  You lose " + str(session["num"]) + " coins.")
+                session["wallet"] += session["num"]
+                flash("You have " + str(session["wallet"]) +" coins in your wallet")
+                print("You have " + str(session["wallet"]) +" coins in your wallet")
+                return redirect("/")
+            elif session["num"] > 0:
+                flash("You lucky Dog.  You win " + str(session["num"]) + " coins.")
+                print("You lucky Dog.  You win " + str(session["num"]) + " coins.")
+                session["wallet"] += session["num"]
+                flash("You have " + str(session["wallet"]) +" coins in your wallet")
+                print("You have " + str(session["wallet"]) +" coins in your wallet")
+                return redirect("/")
+            elif session["num"] == 0:
+                flash("You broke even.  No coins for you")
+                print("You broke even.  No coins for you")
+                session["wallet"] += session["num"]
+                flash("You have " + str(session["wallet"]) +" coins in your wallet")
+                print("You have " + str(session["wallet"]) +" coins in your wallet")
+                return redirect("/")
 
-    if session["wallet"] < 0:
-        flash("Now you have no more money.  The casino people and loan sharks are going to track you down and break your knees and your family will live in poverty.  Were all those drunken nights at the casino really worth it?  You were such an honorable ninja, and now you have nothing.")
-        print("Now you have no more money.  The casino people and loan sharks are going to track you down and break your knees and your family will live in poverty.  Were all those drunken nights at the casino really worth it?  You were such an honorable ninja, and now you have nothing.")
-        return redirect("/")
+    
 
     else:    
         return redirect("/")
